6e32118e1fdb7fd134b1ee84da83dc695e97cdc3
suhilmi01 <suhilmi01@mail.com>
2026-02-24 16:49:49 +0700

    pesan

---
+++ persegi.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+
+{//start
+    float p,l,luas;
+
+    //langkah 2 : masukkan panjang 
+    cout << "masukkan panjang : " ;
+    cin >> p;
+    //langkah 3 : masukkan lebar
+    cout << "masukkan lebar : " ;
+    cin >> l;
+    //langkah 4 : hitung luas = p x l
+    luas = p*l;
+    //langkah 5 :
+    cout << "hasilnya = " << luas ;
+}//selesai
