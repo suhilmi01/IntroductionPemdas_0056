3719a1945dd1feed9fbdde00ee8afc02136a3760
suhilmi01 <suhilmi01@mail.com>
2026-02-24 16:46:52 +0700

    pesan

---
+++ cap.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    bool cek = true;
+    char jk;
+    int angka = 10;
+    float tinggi = 180.5;
+    double harga = 200000.00;
+    string nama = "andi";
+
+    jk = 'L';
+    cout << "selamat datang" << endl;
+    cout << "di TI UMY\n";
+
+    nama = "alex";
+    cout << "nama saya : " << nama;
+
+}
