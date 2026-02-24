60478653e907600cb595595f49a083f6870c535b
suhilmi01 <suhilmi01@mail.com>
2026-02-24 16:01:32 +0700

    first commit : menambahkan file coba.cpp

---
+++ alex.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+//program utama
+int main()
+{
+    //menampilkan variabel umur
+    int umur;
+    //memberi nilai dari variabel
+    umur = 20;
+
+    //menampilkan isi string
+    cout <<"andi" << endl;
+    //menampilkan isi dari variabel
+    cout << umur << endl;
+}
